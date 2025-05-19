04bc8d02fe109d93663fc3ab6de458e0bf1ac849
quang <vuquang01dl@gmail.com>
2025-05-19 19:44:30 +0700

    update5

--- mvc/BookingTour/BookingTour/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ mvc/BookingTour/BookingTour/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afe68e23300f2633f4ef939b92e50103ff6545f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f028ed82eea318444ac012982b3690425cd811eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- mvc/BookingTour/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ mvc/BookingTour/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afe68e23300f2633f4ef939b92e50103ff6545f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f028ed82eea318444ac012982b3690425cd811eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
